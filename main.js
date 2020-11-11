a297afa8e7e94733396c62525402f70a253b2535
Saket Roy <saket.roy@hotmail.com>
2020-11-12 01:40:41 +0530

    fully redesigned responsive website

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ $(function () {
     $("html,body").animate({ scrollTop: $("#section2").offset().top }, "slow");
   });
 });
+
+$(window).on("load", () => {
+  // Animate loader off screen
+  $(".se-pre-con").fadeOut("slow");
+});
