ecac194082fbee37be137c11ce7157f6180e2ea0
Saket Roy <saket.roy@hotmail.com>
2020-11-11 12:55:47 +0530

    Add initial cards for testimonials

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 $(function () {
-  $(".bg").height($(window).height());
-
   $("a.btn-scroll-down").on("click", () => {
     $("html,body").animate({ scrollTop: $("#section2").offset().top }, "slow");
   });
